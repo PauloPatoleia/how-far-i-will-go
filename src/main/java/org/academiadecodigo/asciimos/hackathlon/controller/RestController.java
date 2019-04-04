dfaeb31ad4c1cfa4eda28be5a7af6fa8c7139ed8
lbertoncin <geral@lucasbertoncin.com>
2019-04-05 00:48:24 +0100

    add cross-origin

--- src/main/java/org/academiadecodigo/asciimos/hackathlon/controller/RestController.java
+++ src/main/java/org/academiadecodigo/asciimos/hackathlon/controller/RestController.java
@@ -8,12 +8,14 @@ import org.academiadecodigo.asciimos.hackathlon.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+@CrossOrigin(value = "*")
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("api")
 public class RestController {
